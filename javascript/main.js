261d15cd75652a791fc3fef92ca183a10126abb0
Svinci131 <svinci131@gmail.com>
2015-11-06 21:22:39 -0500

    project euler prob#1

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+var arr = [ 1, 2, 3, 4,5,6]; 
+
+
+
+var literal = {}; 
+
+
+
+for (var i = 0; i < arr.length; i++){ 
+  //console.log(i)
+    for (var j = 10; j >= 1; j--){
+        var current = arr[ i ] * j;
+        if ( typeof literal[ current ] === "undefined" ) {
+          literal[ current ] = 1;
+        }
+        else {
+          literal[ current ] += 1;
+        }
+        if ( literal[ current ] === 3 ) {
+          console.log( 'ANSWER: ' + current, literal[ current ] );
+          break;
+        }
+    
+
+  }
+}
+
+console.log(literal)
