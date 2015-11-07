7db34c6919674910c0e7db12643f1446094a84aa
Svinci131 <svinci131@gmail.com>
2015-11-06 23:57:39 -0500

    committing

--- javascript/v1.js
+++ javascript/v1.js
@@ -1,4 +1,5 @@
     var array = [1,2,3,4,5,6,7,8,9,10]
+    
   //var numbers =[1];
     for (var i = 0; i <= array.length; i++) {
   //GO THROUGH ARRAY
--- javascript/v2.js
+++ javascript/v2.js
@@ -1,25 +1,26 @@
-<<<<<<< HEAD:javascript/v2.js
 var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
-=======
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
->>>>>>> bb3061dca9cd027fcb97c4ff281941838ad403be:javascript/main.js
 
-// Find the sum of all the multiples of 3 or 5 below 1000.
 
-var multiples = [];
 
-for (var i = 999; i > 0; --i) {
+var literal = {}; 
 
-  if (i % 3 === 0 || i % 5 === 0) {
-    multiples.push(i)
+
+
+for (var i = 0; i < arr.length; i++){ 
+  //console.log(i)
+    for (var j = 10; j >= 1; j--){
+        var current = arr[ i ] * j;
+        if ( typeof literal[ current ] === "undefined" ) {
+          literal[ current ] = 1;
+        }
+        else {
+          literal[ current ] += 1;
+        }
+        if ( literal[ current ] === 3 ) {
+          console.log( 'ANSWER: ' + current, literal[ current ] );
+          break;
+        }
+    
+
   }
-<<<<<<< HEAD:javascript/v2.js
 }
-=======
-}
-
-var sum = eval(multiples.join('+'))
-console.log(sum)
-
-
->>>>>>> bb3061dca9cd027fcb97c4ff281941838ad403be:javascript/main.js
